4f99eee2eb83d9d8f2202469a503ba5e95e73ed6
ANtibushitse <ntibushitse.arsene@gmail.com>
2016-10-09 08:54:13 +0200

    Step II  términé

--- index-step2.js
+++ index-step2.js
@@ -1,32 +1,32 @@
 $(document).ready(pied);
-
 // Fonction principale
-function pied(){
+function pied() {
+	function clickValider() {
+		var wholeNumber = $("input").val();
 
-	// == Fonction clickValider == 
-	// Créer la fonction 'clickValider'
+		function wH(min, max) {
+			min= Math.ceil(min); 
+			max= Math.floor(max);
+			return Math.floor(Math.random()*( max- min+1))+1  
+		}
+		var foo = wH(42, 77);
+		console.log(foo);
+	if ( wholeNumber == wH) 
+		{alert("you WON")
+	else   (wholeNumber > wH){
+		alert("you LOSE")
+	}
 
-		// Récupérer le contenu de mon input
-
-		// Stocker un nombre ENTIER généré "aléatoirement"
-
-		// Comparer ce contenu à ce nombre
-			// Si le contenu est égal
-			// Afficher une alert Gagné
-
-			// Sinon s'il est supérieur
-			// Afficher une alert Perdu, votre nombre est trop grand
-
-			// Sinon s'il est inférieur
-			// Afficher une alert Perdu, votre nombre est trop petit
-			
-			
-	// /Fin == Fonction clickValider == 
-
-
-
+		// Si le contenu est égal
+		// Afficher une alert Gagné
+		// Sinon s'il est supérieur
+		// Afficher une alert Perdu, votre nombre est trop grand
+		// Sinon s'il est inférieur
+		// Afficher une alert Perdu, votre nombre est trop petit
+	}
+	clickValider();  // /Fin == Fonction clickValider ==
+	 $("button").click(function () {
+		clickValider();
 	// Ecrire le code qui va détecter que le bouton 'valider' est cliqué.
 	// Et qui appelle la fonction 'clickValider' défini ci-dessus
-
-
 }
